33a6519e8544fb0c5fa9377e88735ee6d5417ba6
Darran Hough <115110194+darran-hough@users.noreply.github.com>
2024-04-03 14:02:43 +0100

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -119,12 +119,13 @@ sudo adduser $USER audio
 # This is required for yabridge
 # See https://wiki.winehq.org/Ubuntu and https://wiki.winehq.org/Winetricks for additional information.
 # ---------------------------
-sudo dpkg --add-architecture i386
+sudo dpkg --add-architecture i386 
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-sudo apt update
-sudo apt install --install-recommends winehq-staging -y
+sudo apt update && sudo apt install --install-recommends winehq-stable
+
+
 
 # ---------------------------
 # Install Winetricks
