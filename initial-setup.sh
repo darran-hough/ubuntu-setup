3e10da6df7b506aeeff663c4d85315053a5942cd
Darran Hough <115110194+darran-hough@users.noreply.github.com>
2024-04-04 14:35:34 +0100

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -21,16 +21,19 @@ notify () {
 # ---------------------------
 sudo apt update && sudo apt upgrade && sudo apt dist-upgrade -y
 
+
 # ---------------------------
 # GPU AMD Drivers
 # ---------------------------
 wget https://repo.radeon.com/amdgpu-install/23.40.2/ubuntu/focal/amdgpu-install_6.0.60002-1_all.deb
 sudo dpkg -i amdgpu-install_6.0.60002-1_all.deb
 
+
 # ---------------------------
 # Update Snap Store
 # ---------------------------
 snap-store --quit && sudo snap refresh snap-store
+
 
 # ---------------------------
 # Change to low latency Kernel
@@ -38,9 +41,8 @@ snap-store --quit && sudo snap refresh snap-store
 sudo apt install linux-image-lowlatency-hwe-22.04
 sudo apt remove linux-image-generic-hwe-22.04 
 
-# ---------------------------
-# KXStudio
-# ---------------------------
+
+# ================================ KXStudio
 # Update software sources
 sudo apt-get update
 # Install required dependencies if needed
@@ -51,21 +53,33 @@ wget https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-re
 sudo dpkg -i kxstudio-repos_11.1.0_all.deb
 sudo apt update
 sudo apt install cadence
+
 sudo apt update && sudo apt upgrade && sudo apt dist-upgrade -y
 
+
 # ---------------------------
-# Pipewire
+# Install PipeWire
 # ---------------------------
+# 1. **Check if PipeWire is installed**: PipeWire is pre-installed out-of-the-box in Ubuntu 22.04 and runs as a background service automatically. You can check its status by running the following command in the terminal:
+# systemctl --user status pipewire pipewire-session-manager
+# 2. **Install client libraries**: Although PipeWire is available out-of-the-box, it's not in use by default for audio output. To get started, open the terminal (Ctrl+Alt+T) and run the following command to install the client libraries:
 sudo apt install pipewire-audio-client-libraries libspa-0.2-bluetooth libspa-0.2-jack
-sudo apt update
+# 3. **Install wireplumber to replace pipewire-media-session**: The project maintainer now recommends the more advanced "wireplumber" session manager when using PipeWire as the system sound server. To install the package and remove "pipewire-media-session", run the following command in the terminal:
 sudo apt install wireplumber pipewire-media-session-
+# Note: There's a '-' at the end of the command which indicates to remove the package. The command will also install the required pipewire-pulse automatically.
+# 4. **Copy configuration files**: For ALSA clients to be configured to output via PipeWire, run the following command to copy the configuration file:
 sudo cp /usr/share/doc/pipewire/examples/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
+# For JACK client, run the following commands:
 sudo cp /usr/share/doc/pipewire/examples/ld.so.conf.d/pipewire-jack-*.conf /etc/ld.so.conf.d/
 sudo ldconfig
+# For Bluetooth, just remove the pulseaudio-module-bluetooth package via command:
 sudo apt remove pulseaudio-module-bluetooth
+# And, finally enable the media session by running command:
 systemctl --user --now enable wireplumber.service
-#to check status 
-#pactl info
+# After restarting Ubuntu, you can verify the installation by running the command below in terminal. It should output Sound server: PulseAudio (on PipeWire x.x.x) indicates Pipewire is in use as sound output.
+# pactl info
+
+
 
 # ---------------------------
 # Modify GRUB options
@@ -76,9 +90,11 @@ systemctl --user --now enable wireplumber.service
 sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash threadirqs mitigations=off cpufreq.default_governor=performance"/g' /etc/default/grub
 sudo update-grub
 
+
 # ---------------------------
 # sysctl.conf
 # ---------------------------
+notify "sysctl.conf"
 # See https://wiki.linuxaudio.org/wiki/system_configuration for more information.
 echo 'vm.swappiness=10
 fs.inotify.max_user_watches=600000' | sudo tee -a /etc/sysctl.conf
@@ -95,6 +111,10 @@ echo '@audio - rtprio 90
 # ---------------------------
 sudo adduser $USER audio
 
+
+
+
+
 # ---------------------------
 # Wine (staging)
 # This is required for yabridge
@@ -105,6 +125,8 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
 sudo apt update && sudo apt install --install-recommends winehq-stable
+
+
 
 # ---------------------------
 # Install Winetricks
@@ -192,7 +214,8 @@ sudo apt-get install -f
 # ---------------------------
 # Whatsapp
 # ---------------------------
-# sudo snap install whatsapp-for-linux
+#sudo snap install whatsapp-for-linux
+
 
 # ---------------------------
 # Bitwig
@@ -202,6 +225,7 @@ sudo apt-get install -f
 # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 # sudo apt update
 # flatpak install flathub com.bitwig.BitwigStudio
+
 
 # ---------------------------
 # install Chrome & remove Firefox
@@ -221,7 +245,6 @@ sudo apt install piper
 # ---------------------------
 sudo apt update
 sudo apt install -y gimp
-
 
 # ---------------------------
 # Cleanup
